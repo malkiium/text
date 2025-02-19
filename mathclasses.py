9adfdfb81b66afdfdc74442da20f906b97e7c7d3
malkiium <elihalldelafosse@gmail.com>
2025-02-19 23:29:31 +0100

    a

---
+++ mathclasses.py
@@ -0,0 +1,22 @@
+def exosAB():
+    #the exercise zone
+    exosAB1 = str("""
+                2x+3 >= 5x+3 =
+                2x+3 >= 5x+3 =
+                -3x+3 >= 3
+                -3x >= 0
+                -x >= 0
+                -x ∈ [0; +inf[
+                ________________
+                3 >= 3x+3
+                0 >= 3x
+                0 >= x
+                x ∈ ]-inf; 0] """)
+
+def exosPolinome2nd():
+    #the exercise zone
+    exosPol1 = str("""
+                 -x² + 2x + 1 < 0
+                 - """)
+
+
